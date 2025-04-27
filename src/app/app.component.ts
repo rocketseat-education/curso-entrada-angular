6541607f7aa80934af55f8df5fd085107bad841e
vhraposo <victorhcraposo@gmail.com>
2025-04-27 15:37:47 -0300

    Aula 3.5 - Desenvolvendo o componente Primary Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule],
+  imports: [NavbarComponent, CommonModule, PrimaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
