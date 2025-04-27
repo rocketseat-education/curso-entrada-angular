36451fb3812f76e85d8e4d6d69f86d34ccdd98d3
vhraposo <victorhcraposo@gmail.com>
2025-04-27 15:49:34 -0300

    Aula 3.6 - Desenvolvendo o componente Secondary Button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule, PrimaryButtonComponent],
+  imports: [NavbarComponent, CommonModule, PrimaryButtonComponent, SecondaryButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
