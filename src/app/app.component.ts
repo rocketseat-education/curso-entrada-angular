2a6a8ac72aa22e63bbda43bedcfc7bf7d5af7366
vhraposo <victorhcraposo@gmail.com>
2025-05-06 00:34:08 -0300

    Aula 3.9 - Desenvolvendo o aviso da página de lista de certificados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,24 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 import { ItemCertificadoComponent } from './_components/item-certificado/item-certificado.component';
-import { NavbarComponent } from "./_components/navbar/navbar.component";
-import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
+import { NavbarComponent } from './_components/navbar/navbar.component';
+import { PrimaryButtonComponent } from './_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './_components/secondary-button/secondary-button.component';
+import { CertificadosComponent } from './pages/certificados/certificados.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, CommonModule, PrimaryButtonComponent, SecondaryButtonComponent, ItemCertificadoComponent, BaseUiComponent],
+  imports: [
+    NavbarComponent,
+    CommonModule,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    ItemCertificadoComponent,
+    BaseUiComponent,
+    CertificadosComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'gerador-certificado';
