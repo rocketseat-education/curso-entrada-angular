c122a83a629cf12bc3bfa3ccb21983e0b056e5bd
vhraposo <victorhcraposo@gmail.com>
2025-05-20 23:00:26 -0300

    Aula 4.3 - Usando o Router para navegação via typescript

--- src/app/_components/item-certificado/item-certificado.component.ts
+++ src/app/_components/item-certificado/item-certificado.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { SecondaryButtonComponent } from '../secondary-button/secondary-button.component';
 
 @Component({
   selector: 'app-item-certificado',
-  imports: [SecondaryButtonComponent, RouterLink],
+  imports: [SecondaryButtonComponent],
   templateUrl: './item-certificado.component.html',
   styleUrl: './item-certificado.component.css',
 })
 export class ItemCertificadoComponent {
   id: string = '6';
+  constructor(private router: Router) {}
+
+  redirecionaCertificado() {
+    this.router.navigate(['/certificados', 2]);
+  }
 }
