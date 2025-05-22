95547b4049b15f7b31ff0c860ff1371c3211c62e
vhraposo <victorhcraposo@gmail.com>
2025-05-22 02:25:17 -0300

    Aula 4.8 - Criando uma interface para certificado

--- src/app/pages/certificados-form/certificados-form.component.ts
+++ src/app/pages/certificados-form/certificados-form.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { PrimaryButtonComponent } from '../../_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from '../../_components/secondary-button/secondary-button.component';
+import { Certificado } from '../../interfaces/certificado';
 
 @Component({
   selector: 'app-certificados-form',
@@ -16,24 +17,35 @@ import { SecondaryButtonComponent } from '../../_components/secondary-button/sec
   styleUrl: './certificados-form.component.css',
 })
 export class CertificadosFormComponent {
-  nome: string = '';
+  certificado: Certificado = {
+    atividades: [],
+    nome: '',
+  };
   atividade: string = '';
-  atividades: string[] = [];
 
   campoInvalido(control: NgModel) {
     return control.invalid && control.touched;
   }
 
   formValido() {
-    return this.atividades.length > 0 && this.nome.length > 0;
+    return (
+      this.certificado.atividades.length > 0 && this.certificado.nome.length > 0
+    );
   }
 
   adicionarAtividade() {
-    this.atividades.push(this.atividade);
+    this.certificado.atividades.push(this.atividade);
     this.atividade = '';
   }
 
   excluirAtividade(index: number) {
-    this.atividades.splice(index, 1);
+    this.certificado.atividades.splice(index, 1);
+  }
+
+  submit() {
+    if (!this.formValido()) {
+      return;
+    }
+    console.log(this.certificado);
   }
 }
